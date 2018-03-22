1047631ad39103d3ad930f31bee8d5a71c938943
fmacti <fer.aeq@gmail.com>
2018-03-22 16:00:09 -0300

    Criando operacao de instanciacao

--- src/main/java/com/fmac/cursomc/CursomcApplication.java
+++ src/main/java/com/fmac/cursomc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.fmac.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fmac.cursomc.domain.Categoria;
+import com.fmac.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "C#");
+		Categoria cat2 = new Categoria(null, "Java");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
